2102651cbeecfd7b97dd13cc9b221b945ba0fd63
Lucas Neuhaus <lucas.neuhaus93@gmail.com>
2020-06-21 14:16:53 -0300

    Section 8 completed!

---
+++ project-2/src/components/SearchBar.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { View, TextInput, StyleSheet } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+interface Props {
+  term: string;
+  onTermChange: (newTerm: string) => void;
+  onTermSubmit: () => void;
+}
+
+const SearchBar = (props: Props) => {
+  const { term, onTermChange, onTermSubmit } = props;
+
+  return (
+    <View style={styles.backgroundStyle}>
+      <Feather name="search" style={styles.iconStyle} />
+      <TextInput
+        autoCorrect={false}
+        placeholder="Search"
+        style={styles.inputStyle}
+        value={term}
+        onChangeText={(newTerm) => onTermChange(newTerm)}
+        onEndEditing={() => onTermSubmit()}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    backgroundColor: "#F0EEEE",
+    marginTop: 10,
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: "row",
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center",
+    marginHorizontal: 15,
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18,
+  },
+});
+
+export default SearchBar;
